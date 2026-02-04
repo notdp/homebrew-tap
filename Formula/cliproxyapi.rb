04c4f876de08cd00a0b21913fae8d45e0bf44628
dp <dp0x7ce@gmail.com>
2026-02-04 18:43:57 +0800

    Add cliproxyapi formula with credential-master support

---
+++ Formula/cliproxyapi.rb
@@ -0,0 +1,54 @@
+class Cliproxyapi < Formula
+  desc "CLI Proxy API with credential-master support for multi-instance OAuth token coordination"
+  homepage "https://github.com/notdp/CLIProxyAPI"
+  version "6.7.20-credential-master"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/notdp/CLIProxyAPI/releases/download/v6.7.20-credential-master/cliproxyapi-darwin-arm64"
+      sha256 "8ff427dd13939a961882804cb672a4dfc0430699414f35dee5d043f439c72490"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/notdp/CLIProxyAPI/releases/download/v6.7.20-credential-master/cliproxyapi-linux-amd64"
+      sha256 "3fbb38c69bcc6fd04640d33280c68cdee3c5dfca81b61312078696694d52ae63"
+    end
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "cliproxyapi-darwin-arm64" => "cliproxyapi"
+    elsif OS.linux? && Hardware::CPU.intel?
+      bin.install "cliproxyapi-linux-amd64" => "cliproxyapi"
+    end
+  end
+
+  service do
+    run [opt_bin/"cliproxyapi"]
+    keep_alive true
+    log_path var/"log/cliproxyapi.log"
+    error_log_path var/"log/cliproxyapi.log"
+    working_dir var/"cliproxyapi"
+  end
+
+  def caveats
+    <<~EOS
+      配置文件位置: /opt/homebrew/etc/cliproxyapi.conf
+
+      Credential-master 配置示例 (follower):
+        credential-master: "http://master-ip:8888"
+        remote-management:
+          secret-key: "<与 master 相同的 hash>"
+
+      启动服务:
+        brew services start cliproxyapi
+    EOS
+  end
+
+  test do
+    system "#{bin}/cliproxyapi", "--version"
+  end
+end
